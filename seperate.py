edd36516f42d4d9dc75120833c4f32cefcc6b754
kkr1947 <59726644+kkr1947@users.noreply.github.com>
2020-09-17 17:36:31 +0530

    jupyter notebook to vs code version

---
+++ seperate.py
@@ -0,0 +1,78 @@
+import pandas as pd  
+import numpy as np
+
+df = pd.read_csv("./new.csv")
+
+# Extract contact us page from homepage
+def contact_page(st):
+    li = st.split(',')
+
+        
+    ls = [match for match in li if "contact" in match]
+    if len(ls):
+        ls = ls[0]
+        return ls
+    else:
+        return ("")
+# df['hlink']+
+df['contact'] = df[df['heading'].apply(contact_page).str.contains('contact')]['hlink']+df[df['heading'].apply(contact_page).str.contains('contact')]['heading'].apply(contact_page).apply(lambda x:x[1:])
+
+# Extracting facebook leads from a tags
+def face_extract(st):
+    li = st.split(',')
+
+        
+    ls = [match for match in li if "facebook" in match]
+    if len(ls):
+        ls = ls[0]
+        return ls
+    else:
+        return ("")
+
+df['facebook'] = df[df['heading'].apply(face_extract).str.contains('facebook')]['heading'].apply(face_extract)
+
+
+# Extracting instagram leads from a tags
+def insta_extract(st):
+    li = st.split(',')
+
+        
+    ls = [match for match in li if "instagram" in match]
+    if len(ls):
+        ls = ls[0]
+        return ls
+    else:
+        return ("")
+
+df['instagram'] = df[df['heading'].apply(insta_extract).str.contains('instagram')]['heading'].apply(insta_extract)
+
+
+# Extracting youtube leads from a tags
+def youtube_extract(st):
+    li = st.split(',')
+
+        
+    ls = [match for match in li if "youtube" in 
+          match]
+    if len(ls):
+        ls = ls[0]
+        return ls
+    else:
+        return ("")
+df['youtube'] = df[df['heading'].apply(youtube_extract).str.contains('youtube')]['heading'].apply(youtube_extract)
+
+# Extracting emails leads from a tags
+def email_extract(st):
+    li = st.split(',')
+
+        
+    ls = [match for match in li if "@" in match]
+    if len(ls):
+        ls = ls[0]
+        return ls
+    else:
+        return ("")
+# df['hlink']+
+df['email'] = kf[kf['heading'].apply(email_extract).str.contains('@')]['heading'].apply(email_extract).apply(lambda x: x[7:])
+
+df.to_csv('final_data.csv', index=False) #writing data
